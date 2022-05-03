ee7c64f6b5e9982bffb37eacdd60068761d3bcab
Vsevolod Pavlovskyi <vpavlovskyi@icloud.com>
2022-05-03 17:56:07 +0300

    Initial Commit

---
+++ DecodableUICongifurationEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DecodableUICongifurationEditor
+//
+//  Created by Vsevolod Pavlovskyi on 03.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DecodableUICongifurationEditor/DecodableUICongifurationEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodableUICongifurationEditorApp.swift
+//  DecodableUICongifurationEditor
+//
+//  Created by Vsevolod Pavlovskyi on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DecodableUICongifurationEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
